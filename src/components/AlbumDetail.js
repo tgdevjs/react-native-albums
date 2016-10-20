1eaa9cc452053bde0836dcbc573fbc3267c1e5b0
tg <tgdevjs@gmail.com>
2016-10-20 15:27:33 -0400

    Added album image and styles for AlbumDetail component.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,34 +5,59 @@ import Card from  './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
-  const { title, artist, thumbnail_image } = props.album
-  const {thumbnailStyle, headerContentStyle } = styles;
+  const { title, artist, thumbnail_image, image } = props.album
+  const {
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerContentStyle,
+    headerTextStyle,
+    imageStyle} = styles;
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{uri: thumbnail_image}}
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>Title: {title}</Text>
-          <Text>Artist: {artist}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+          <Image
+            style={imageStyle}
+            source={{uri: image }}
+          />
       </CardSection>
     </Card>
   )
 }
 
 styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
   thumbnailStyle: {
     height: 50,
     width: 50,
   },
-  headerContentStyle: {
-    flexDirection: 'column',
-    justifyContent: 'space-around'
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    flex: 1,
+    height: 300,
+    width: null,
   }
 }
 
