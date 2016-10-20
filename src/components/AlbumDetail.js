e68135b40ea2a0c51ef4d6c209f25c950b8eab41
tg <tgdevjs@gmail.com>
2016-10-20 15:06:16 -0400

    Arrange and add styling for content header inside AlbumDetail.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import Card from  './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
-  const { title, artist } = props.album
+  const { title, artist, thumbnail_image } = props.album
+  const {thumbnailStyle, headerContentStyle } = styles;
   return (
     <Card>
-      <Text>
-        Title: {title}
-      </Text>
-      <Text>
-        Artist: {artist}
-      </Text>
+      <CardSection>
+        <View>
+          <Image
+            style={thumbnailStyle}
+            source={{uri: thumbnail_image}}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text>Title: {title}</Text>
+          <Text>Artist: {artist}</Text>
+        </View>
+      </CardSection>
     </Card>
   )
+}
+
+styles = {
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  }
 }
 
 export default AlbumDetail;
